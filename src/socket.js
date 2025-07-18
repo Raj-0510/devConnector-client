6ef4ff8f524132d31839a2c948e96f2dd69d1ab2
Raj Jayswal <rajjayswal480@gmail.com>
2025-07-18 18:29:18 +0530

    Socket url, second change

--- src/socket.js
+++ src/socket.js
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 // const socket = io("http://localhost:5000",{
 //   withCredentials: true
 // });
-const socket = io("https://dev-connector-client1.netlify.app",{
+const socket = io("https://devconnector-1-backend.onrender.com",{
   withCredentials: true
 });
 export default socket;
