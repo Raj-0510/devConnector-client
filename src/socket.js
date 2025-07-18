21500d67c279692c687d099bb62aa44c794c11f3
Raj Jayswal <rajjayswal480@gmail.com>
2025-07-18 18:19:14 +0530

    Socket add. change

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,9 @@
 // socket.js
 import { io } from "socket.io-client";
-const socket = io("http://localhost:5000",{
+// const socket = io("http://localhost:5000",{
+//   withCredentials: true
+// });
+const socket = io("https://dev-connector-client1.netlify.app",{
   withCredentials: true
 });
 export default socket;
